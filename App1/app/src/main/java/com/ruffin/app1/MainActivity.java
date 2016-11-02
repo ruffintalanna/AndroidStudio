497716f99c8027afd1b5f3e316de29367fab923e
Talanna Ruffin <talannaruffin@Talannas-MacBook-Air.local>
2016-11-02 15:47:08 -0400

    added User Info screen

--- App1/app/src/main/java/com/ruffin/app1/MainActivity.java
+++ App1/app/src/main/java/com/ruffin/app1/MainActivity.java
@@ -1,13 +1,35 @@
 package com.ruffin.app1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mUserInfo, mHandTremor, mHeartBeat, mSendInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUserInfo = (Button) findViewById(R.id.btn_userInfo);
+        mHandTremor = (Button) findViewById(R.id.btn_handTremors);
+        mHeartBeat = (Button) findViewById(R.id.btn_handTremors);
+        mSendInfo = (Button) findViewById(R.id.btn_sendInfo);
+
+        mUserInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startUserInfo();
+            }
+        });
+    }
+
+    private void startUserInfo() {
+        Intent intent = new Intent(this, UserInfo.class);
+        startActivity(intent);
     }
 }
