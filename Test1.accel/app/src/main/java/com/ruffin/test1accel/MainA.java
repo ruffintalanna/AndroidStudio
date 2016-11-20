b9b2febf0c60689e7efd9d4d58ea9541550ec0c3
Talanna Ruffin <talannaruffin@destiny.nsu.edu>
2016-11-20 10:51:22 -0500

    Test1.accel

---
+++ Test1.accel/app/src/main/java/com/ruffin/test1accel/MainA.java
@@ -0,0 +1,49 @@
+package com.ruffin.test1accel;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.ruffin.test1accel.R;
+
+public class MainA extends AppCompatActivity implements SensorEventListener {
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 600;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    public void
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+    @Override
+    public void onAccuracyChanged (Sensor sensor, int accuracy) {
+
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        senSensorManager.unregisterListener(this);
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+}
